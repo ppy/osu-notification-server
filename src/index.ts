d24c9611177ddb21affd4eec348fe3a7582c6415
nanaya <me@nanaya.pro>
2019-03-14 16:47:33 +0900

    Split off oauth verifier

--- src/index.ts
+++ src/index.ts
@@ -17,26 +17,15 @@
  */
 
 import * as dotenv from 'dotenv';
-import * as fs from 'fs';
 import * as http from 'http';
-import * as jwt from 'jsonwebtoken';
 import * as mysql from 'mysql2/promise';
 import * as path from 'path';
 import 'source-map-support/register';
-import * as url from 'url';
 import * as WebSocket from 'ws';
 import LaravelSession from './laravel-session';
+import OAuthVerifier from './oauth-verifier';
 import RedisSubscriber from './redis-subscriber';
 import UserConnection from './user-connection';
-
-interface OAuthJWT {
-  aud: string;
-  jti: string;
-  iat: number;
-  nbf: number;
-  sub: string;
-  scopes: string[];
-}
 
 let baseDir = process.env.WEBSOCKET_BASEDIR;
 
@@ -74,76 +63,14 @@ const laravelSession = new LaravelSession({
   port: process.env.REDIS_PORT == null ? 6379 : +process.env.REDIS_PORT,
 });
 
-const oAuthTokenSignatureKey = fs.readFileSync(`${baseDir}/oauth-public.key`);
-const isOAuthJWT = (arg: object|string): arg is OAuthJWT => {
-  return typeof arg === 'object';
-};
-
-const getOAuthToken = (req: http.IncomingMessage) => {
-  let token;
-  const authorization = req.headers.authorization;
-
-  // no authorization header, try from query string
-  if (authorization == null) {
-    if (req.url == null) {
-      return;
-    }
-
-    const params = url.parse(req.url, true).query;
-
-    if (typeof params.access_token === 'string') {
-      token = params.access_token;
-    }
-  } else {
-    const matches = authorization.match(/^Bearer (.+)$/);
-
-    if (matches != null) {
-      token = matches[1];
-    }
-  }
-
-  if (token == null) {
-    return;
-  }
-
-  const parsedToken = jwt.verify(token, oAuthTokenSignatureKey);
-
-  if (isOAuthJWT(parsedToken)) {
-    return parsedToken.jti;
-  }
-};
-
-const verifyOAuthToken = async (req: http.IncomingMessage) => {
-  const oAuthToken = getOAuthToken(req);
-
-  if (oAuthToken == null) {
-    return;
-  }
-
-  const [rows, fields] = await db.execute(`
-    SELECT user_id, scopes
-    FROM oauth_access_tokens
-    WHERE revoked = false AND expires_at > now() AND id = ?
-  `, [
-    oAuthToken,
-  ]);
-
-  if (rows.length === 0) {
-    throw new Error('authentication failed');
-  }
-
-  const userId = rows[0].user_id;
-  const scopes = JSON.parse(rows[0].scopes);
-
-  for (const scope of scopes) {
-    if (scope === '*' || scope === 'read') {
-      return userId;
-    }
-  }
-};
+const oAuthVerifier = new OAuthVerifier({
+  baseDir,
+  db,
+});
 
 const getUserId = async (req: http.IncomingMessage) => {
-  let userId = await verifyOAuthToken(req);
+  let userId = await oAuthVerifier.verifyRequest(req);
+
   if (userId == null) {
     userId = await laravelSession.verifyRequest(req);
   }
---
+++ src/oauth-verifier.ts
@@ -0,0 +1,114 @@
+/**
+ *    Copyright (c) ppy Pty Ltd <contact@ppy.sh>.
+ *
+ *    This file is part of osu!web. osu!web is distributed with the hope of
+ *    attracting more community contributions to the core ecosystem of osu!.
+ *
+ *    osu!web is free software: you can redistribute it and/or modify
+ *    it under the terms of the Affero GNU General Public License version 3
+ *    as published by the Free Software Foundation.
+ *
+ *    osu!web is distributed WITHOUT ANY WARRANTY; without even the implied
+ *    warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *    See the GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with osu!web.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import * as fs from 'fs';
+import * as http from 'http';
+import * as jwt from 'jsonwebtoken';
+import * as mysql from 'mysql2/promise';
+import * as url from 'url';
+
+interface Config {
+  baseDir: string;
+  db: mysql.Pool;
+}
+
+interface OAuthJWT {
+  aud: string;
+  jti: string;
+  iat: number;
+  nbf: number;
+  sub: string;
+  scopes: string[];
+}
+
+const isOAuthJWT = (arg: object|string): arg is OAuthJWT => {
+  return typeof arg === 'object';
+};
+
+export default class OAuthVerifier {
+  config: Config;
+  oAuthTokenSignatureKey: Buffer;
+
+  constructor(config: Config) {
+    this.config = config;
+    this.oAuthTokenSignatureKey = fs.readFileSync(`${this.config.baseDir}/oauth-public.key`);
+  }
+
+  getToken = (req: http.IncomingMessage) => {
+    let token;
+    const authorization = req.headers.authorization;
+
+    // no authorization header, try from query string
+    if (authorization == null) {
+      if (req.url == null) {
+        return;
+      }
+
+      const params = url.parse(req.url, true).query;
+
+      if (typeof params.access_token === 'string') {
+        token = params.access_token;
+      }
+    } else {
+      const matches = authorization.match(/^Bearer (.+)$/);
+
+      if (matches != null) {
+        token = matches[1];
+      }
+    }
+
+    if (token == null) {
+      return;
+    }
+
+    const parsedToken = jwt.verify(token, this.oAuthTokenSignatureKey);
+
+    if (isOAuthJWT(parsedToken)) {
+      return parsedToken.jti;
+    }
+  }
+
+  verifyRequest = async (req: http.IncomingMessage) => {
+    const oAuthToken = this.getToken(req);
+
+    if (oAuthToken == null) {
+      return;
+    }
+
+    const [rows, fields] = await this.config.db.execute(`
+      SELECT user_id, scopes
+      FROM oauth_access_tokens
+      WHERE revoked = false AND expires_at > now() AND id = ?
+    `, [
+      oAuthToken,
+    ]);
+
+    if (rows.length === 0) {
+      throw new Error('authentication failed');
+    }
+
+    const userId = rows[0].user_id;
+    const scopes = JSON.parse(rows[0].scopes);
+
+    for (const scope of scopes) {
+      if (scope === '*' || scope === 'read') {
+        return userId;
+      }
+    }
+  }
+}
