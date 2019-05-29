0dda91c8d6a9c8371ae03f6e0aa5a21240f6f78f
nanaya <me@nanaya.pro>
2019-05-29 16:43:18 +0900

    Subscribe to general watch list

--- src/user-connection.ts
+++ src/user-connection.ts
@@ -131,10 +131,12 @@ export default class UserConnection {
     const forumTopic = this.forumTopicSubscriptions();
     const beatmapset = this.beatmapsetSubscriptions();
     const chatChannels = this.chatSubscriptions();
+    const watches = this.watches();
 
     ret.push(...await forumTopic);
     ret.push(...await beatmapset);
     ret.push(...await chatChannels);
+    ret.push(...await watches);
     ret.push(this.userProfileChannel());
     ret.push(`notification_read:${this.session.userId}`);
     ret.push(this.subscriptionUpdateChannel());
@@ -201,6 +203,29 @@ export default class UserConnection {
 
     return rows.map((row) => {
       return `new:forum_topic:${row.topic_id}`;
+    });
+  }
+
+  private watches = async () => {
+    let rows: mysql.RowDataPacket[];
+
+    try {
+      [rows] = await this.db.execute<mysql.RowDataPacket[]>(`
+        SELECT notifiable_type, notifiable_id, subtype
+        FROM watches
+        WHERE user_id = ?
+      `, [this.session.userId]);
+    } catch (err) {
+      // TODO: remove once migrated
+      if (err.code === 'ER_NO_SUCH_TABLE') {
+        return [];
+      }
+
+      throw err;
+    }
+
+    return rows.map((row: any) => {
+      return `new:${row.notifiable_type}:${row.notifiable_id}:${row.subtype}`;
     });
   }
 }
