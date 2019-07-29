60d1939e359bddabae4e0a499f86043719139d42
nanaya <me@nanaya.pro>
2019-07-29 17:58:28 +0900

    No need to check session key

--- src/user-connection.ts
+++ src/user-connection.ts
@@ -123,9 +123,7 @@ export default class UserConnection {
         }
         break;
       case 'verification_requirement_change':
-        if (message.data.key === this.session.key) {
-          this.session.requiresVerification = message.data.requires_verification;
-        }
+        this.session.requiresVerification = message.data.requires_verification;
         break;
       case 'verified':
         if (message.data.key === this.session.key) {
